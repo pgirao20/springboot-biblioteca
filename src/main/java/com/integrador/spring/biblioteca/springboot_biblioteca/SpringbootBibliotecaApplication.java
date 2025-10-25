b956bdca0fd5cc82054c839aa319e96ac87c4724
pgirao20 <pablohgb20@gmail.com>
2025-10-24 19:39:03 -0500

    se agrega el proyecto

---
+++ src/main/java/com/integrador/spring/biblioteca/springboot_biblioteca/SpringbootBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.spring.biblioteca.springboot_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integrador/spring/biblioteca/springboot_biblioteca/SpringbootBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.spring.biblioteca.springboot_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
